b4855fd3d374df2fa24782a4efbb16b082db718b
mad <marc@balloonary.io>
2020-08-20 22:02:44 +0200

    .

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-var dayInc = 12;
-var xLabelInterval = 40;
+const dayInc = 12;
+const xLabelInterval = 40;
 
 function onLoad() {
 	canvas = document.getElementById("canvas");
 	canvas.width = window.innerWidth - 20;
 	context = canvas.getContext("2d");
 
+	canvas.height = Math.min(240, canvas.height);
 	width = canvas.width - 40;
-	startTime = new Date(2000, 0, 1);
+	startTime = new Date();
+	startTime.setFullYear(startTime.getFullYear() - 8);
 	endTime = new Date();
-	endTime.setDate(endTime.getDate() + 800);
+	endTime.setFullYear(endTime.getFullYear() + 2);
 
 	paint();
 }
@@ -17,21 +19,21 @@ function onLoad() {
 function paint() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.lineWidth = "2";
-	var points = [];
-	var maxY = 0,
+	const points = [];
+	let maxY = 0,
 		minY = 999999;
 
-	var now = new Date();
-	var time = new Date(startTime);
+	const now = new Date();
+	let time = new Date(startTime);
 	do {
-		var jd = calculateJD(time);
-		//var jd = 2452878.910416667;
-		var earth = getEarthHelio(jd);
-		var mars = getMarsHelio(jd);
-		var d = calcDistance(earth[0], earth[1], earth[2], mars[0], mars[1], mars[2]);
+		const jd = calculateJD(time);
+		//const jd = 2452878.910416667;
+		const earth = getEarthHelio(jd);
+		const mars = getMarsHelio(jd);
+		const d = calcDistance(earth[0], earth[1], earth[2], mars[0], mars[1], mars[2]);
 		points.push({
-			x : new Date(time),
-			y : d
+			x: new Date(time),
+			y: d
 		});
 		minY = Math.min(minY, d);
 		maxY = Math.max(maxY, d);
@@ -39,10 +41,10 @@ function paint() {
 	} while (time < endTime);
 
 	context.beginPath();
-	for (var i in points) {
-		var x = timeToX(points[i].x);
+	for (let i in points) {
+		const x = timeToX(points[i].x);
 		//		var x = width * (points[i].x - points[0].x) / (points[points.length - 1].x - points[0].x);
-		var y = canvas.height * (1 - (points[i].y - minY) / (maxY - minY));
+		const y = canvas.height * (1 - (points[i].y - minY) / (maxY - minY));
 		if (i == 0) {
 			context.moveTo(x, y);
 		} else {
@@ -55,19 +57,19 @@ function paint() {
 
 	context.beginPath();
 	context.strokeStyle = "#000000";
-	context.moveTo(0, canvas.height / 2);
-	context.lineTo(width, canvas.height / 2);
+	context.moveTo(0, canvas.height);
+	context.lineTo(width, canvas.height);
 	context.stroke();
 	context.closePath();
 
 	context.beginPath();
-	var time = new Date(startTime);
-	var isOffset = false;
+	time = new Date(startTime);
+	let isOffset = false;
 	do {
-		var x = timeToX(time);
-		context.moveTo(x, canvas.height / 2 - 8);
-		context.lineTo(x, canvas.height / 2 + 8);
-		writeText(formatTime(time), x, canvas.height / 2 + (isOffset ? 22 : 42), x > 40, 12);
+		const x = timeToX(time);
+		context.moveTo(x, canvas.height - 8);
+		context.lineTo(x, canvas.height + 8);
+		writeText(formatTime(time), x, canvas.height - (isOffset ? 12 : 22), x > 40, 12);
 		isOffset = !isOffset;
 		time.setDate(time.getDate() + xLabelInterval * dayInc);
 	} while (time < endTime);
@@ -76,25 +78,25 @@ function paint() {
 
 	context.beginPath();
 	context.strokeStyle = "#00ff00";
-	var x = timeToX(now);
+	const x = timeToX(now);
 	context.moveTo(x, 0);
 	context.lineTo(x, canvas.height);
 	context.stroke();
 	context.closePath();
 
-	writeText(formatTime(now), x, canvas.height - 20, true, 14);
+	writeText(formatTime(now), x, 40, true, 14);
 
-	var jd = calculateJD(now);
-	var earth = getEarthHelio(jd);
-	var mars = getMarsHelio(jd);
-	var d = calcDistance(earth[0], earth[1], earth[2], mars[0], mars[1], mars[2]);
+	const jd = calculateJD(now);
+	const earth = getEarthHelio(jd);
+	const mars = getMarsHelio(jd);
+	const d = calcDistance(earth[0], earth[1], earth[2], mars[0], mars[1], mars[2]);
 	writeText(d.toFixed(2), x, 20, true, 14);
 
 }
 
-function onClick(e) {
+function onMouseMove(e) {
 	paint();
-	var x = e.pageX - canvas.offsetLeft;
+	const x = e.pageX - canvas.offsetLeft;
 	context.beginPath();
 	context.strokeStyle = "#008080";
 	context.moveTo(x, 0);
@@ -102,38 +104,38 @@ function onClick(e) {
 	context.stroke();
 	context.closePath();
 
-	var time = xToTime(x);
-	var jd = calculateJD(time);
-	var earth = getEarthHelio(jd);
-	var mars = getMarsHelio(jd);
-	var d = calcDistance(earth[0], earth[1], earth[2], mars[0], mars[1], mars[2]);
+	const time = xToTime(x);
+	const jd = calculateJD(time);
+	const earth = getEarthHelio(jd);
+	const mars = getMarsHelio(jd);
+	const d = calcDistance(earth[0], earth[1], earth[2], mars[0], mars[1], mars[2]);
 	context.strokeStyle = "#000000";
-	writeText(d.toFixed(2), x, 40, x > 40, 14);
-	writeText(formatTime(time), x, canvas.height - 20, x > 40, 14);
+	writeText(d.toFixed(2), x, 20, x > 40, 14);
+	writeText(formatTime(time), x, 40, x > 40, 14);
 
 }
 
 function timeToX(time) {
-	var x = width * (time.getTime() - startTime.getTime()) / (endTime.getTime() - startTime.getTime());
+	const x = width * (time.getTime() - startTime.getTime()) / (endTime.getTime() - startTime.getTime());
 	return x;
 }
 
 function xToTime(x) {
-	var time = new Date(startTime.getTime() + x * (endTime.getTime() - startTime.getTime()) / width);
+	const time = new Date(startTime.getTime() + x * (endTime.getTime() - startTime.getTime()) / width);
 	return time;
 }
 
 function formatTime(time) {
-	var month = time.getMonth() + 1;
+	let month = time.getMonth() + 1;
 	if (month < 10) {
 		month = "0" + month;
 	}
-	var timeStr = time.getFullYear() + "/" + month + "/" + time.getDate();
+	const timeStr = time.getFullYear() + "/" + month + "/" + time.getDate();
 	return timeStr;
 }
 
 function writeText(text, x, y, isCentered, height) {
-	var dims = context.measureText(text);
+	const dims = context.measureText(text);
 	context.font = height + "pt Calibri";
 	context.fillStyle = "rgba(255, 255, 255, 0.6)";
 	//	context.fillStyle = "#ff0000";
